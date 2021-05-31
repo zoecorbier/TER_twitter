2211442dd8e0e671dcecfc8f8d28c0ba8bf01a5b
RMeuter <36972352+RMeuter@users.noreply.github.com>
2021-06-01 00:05:51 +0200

    Déplcament LDA générale et présentation de l'étude sur le site

--- Site/presentationProjet/urls.py
+++ Site/presentationProjet/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 
 from . import views # import views so we can use them in urls.
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('Etude1', views.etude1, name="etude"),
+    path('Etude', views.etude1, name="etude"),
+    path('LDA', views.detailLDA, name="LDA"),
+    path('Tweet', csrf_exempt(views.receivetweet), name="tweet"),
 ]
